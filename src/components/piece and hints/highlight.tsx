db6a5b6d5b1a082d9879d755362eed1369b4c3ee
Parteek malik <parteekmalik5@gmail.com>
2023-10-01 15:20:15 +0530

    added randomoves,autopromotion,delete unused files
    added randomoves,autopromotion,delete unused files

--- src/components/piece and hints/highlight.tsx
+++ src/components/piece and hints/highlight.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import { squareSize, MovesPlayed_Type } from "../types";
+// import React from "react";
+// import { squareSize, MovesPlayed_Type } from "../types";
 
-export interface HighlightProps {
-  selectedPiece: { isSelected: boolean; row: number; col: number };
-  movesPlayed: MovesPlayed_Type;
-}
+// export interface HighlightProps {
+//   selectedPiece: { isSelected: boolean; row: number; col: number };
+// }
 
-const PrevHighlight: React.FC<MovesPlayed_Type> = ({ current, moves }) => {
-  if (current > -1) {
-    const move = moves[current];
-    let from: { row: number; col: number; piece: string } = move.from;
-    let to: { row: number; col: number; piece: string } = move.to;
+// const PrevHighlight: React.FC<MovesPlayed_Type> = ({ current, moves }) => {
+//   if (current > -1) {
+//     const move = moves[current];
+//     let from: { row: number; col: number; piece: string } = move.from;
+//     let to: { row: number; col: number; piece: string } = move.to;
 
-    return (
-      <>
-        <div className="highlight" style={{ transform: `translate(${from.col * squareSize}px, ${from.row * squareSize}px)` }}></div>
-        <div className="highlight" style={{ transform: `translate(${to.col * squareSize}px, ${to.row * squareSize}px)` }}></div>
-      </>
-    );
-  }
-  return null;
-};
+//     return (
+//       <>
+//         <div className="highlight" style={{ transform: `translate(${from.col * squareSize}px, ${from.row * squareSize}px)` }}></div>
+//         <div className="highlight" style={{ transform: `translate(${to.col * squareSize}px, ${to.row * squareSize}px)` }}></div>
+//       </>
+//     );
+//   }
+//   return null;
+// };
 
-const Highlight: React.FC<HighlightProps> = ({ selectedPiece, movesPlayed }) => {
-  return (
-    <>
-      {selectedPiece.isSelected && (
-        <div className="highlight" style={{ transform: `translate(${selectedPiece.col * squareSize}px, ${selectedPiece.row * squareSize}px)` }}></div>
-      )}
-      <PrevHighlight current={movesPlayed.current} moves={movesPlayed.moves} />
-    </>
-  );
-};
+// const Highlight: React.FC<HighlightProps> = ({ selectedPiece, movesPlayed }) => {
+//   return (
+//     <>
+//       {selectedPiece.isSelected && (
+//         <div className="highlight" style={{ transform: `translate(${selectedPiece.col * squareSize}px, ${selectedPiece.row * squareSize}px)` }}></div>
+//       )}
+//       <PrevHighlight current={movesPlayed.current} moves={movesPlayed.moves} />
+//     </>
+//   );
+// };
 
-export default Highlight;
+// export default Highlight;
+export {};
--- src/components/types.tsx
+++ src/components/types.tsx
@@ -1,120 +1,15 @@
-export interface movesPlayedMove_type {
-  type: string;
+import { PieceSymbol,Square } from "chess.js";
 
-  from: { row: number; col: number; piece: string };
-  to: { row: number; col: number; piece: string };
-  castle?: {
-    from: { row: number; col: number; piece: string };
-    to: { row: number; col: number; piece: string };
-  };
-}
-export interface MovesPlayed_Type {
-  current: number;
-  moves: movesPlayedMove_type[];
-}
 export interface HintsProps {
   isShowHint: boolean;
-  availableMoves: moves_Type[];
+  availableMoves: {from:Square,to:Square,promotion: PieceSymbol | undefined}[];
 }
 export interface selectedPieceProps {
   isSelected: boolean;
   row: number;
   col: number;
 }
-export interface boardData_Type {
-  BoardLayout: string[][];
-  turn: string;
-  movesPlayed: MovesPlayed_Type;
-  iscastle: { [key: string]: { [key: string]: boolean } };
-}
 
-export interface handleMoveProps {
-  boardData: boardData_Type;
-  setBoardData: React.Dispatch<React.SetStateAction<boardData_Type>>;
-  selectedPiece: selectedPieceProps;
-  setSelectedPiece: React.Dispatch<React.SetStateAction<selectedPieceProps>>;
-  setSelectedMoves: React.Dispatch<React.SetStateAction<HintsProps>>;
-  Move: moves_Type;
-}
-
-export interface updatedMovesPlayedProps {
-  selectedPiece: selectedPieceProps;
-  BoardLayout: string[][];
-  movesPlayed: MovesPlayed_Type;
-  Move: moves_Type;
-}
-
-export interface Row_Col_Type {
-  row: number;
-  col: number;
-}
-
-export interface PieceType_Type {
-  pieceType: string;
-}
-
-export interface moves_Type {
-  type: string;
-  row: number;
-  col: number;
-}
-
-export interface Row_Col_PieceType_Type extends Row_Col_Type, PieceType_Type {}
-
-export const pieceMovement: { [key: string]: { row: number; col: number }[] } = {
-  R: [
-    { row: 1, col: 0 },
-    { row: -1, col: 0 },
-    { row: 0, col: 1 },
-    { row: 0, col: -1 },
-  ],
-  B: [
-    { row: 1, col: 1 },
-    { row: -1, col: -1 },
-    { row: 1, col: -1 },
-    { row: -1, col: 1 },
-  ],
-  N: [
-    { row: 2, col: 1 },
-    { row: 2, col: -1 },
-    { row: -2, col: 1 },
-    { row: -2, col: -1 },
-    { row: 1, col: 2 },
-    { row: -1, col: 2 },
-    { row: 1, col: -2 },
-    { row: -1, col: -2 },
-  ],
-  K: [
-    { row: 1, col: 1 },
-    { row: 0, col: 1 },
-    { row: -1, col: 1 },
-    { row: -1, col: 0 },
-    { row: -1, col: -1 },
-    { row: 0, col: -1 },
-    { row: 1, col: -1 },
-    { row: 1, col: 0 },
-  ],
-  Q: [
-    { row: 1, col: 0 },
-    { row: -1, col: 0 },
-    { row: 0, col: 1 },
-    { row: 0, col: -1 },
-    { row: 1, col: 1 },
-    { row: -1, col: -1 },
-    { row: 1, col: -1 },
-    { row: -1, col: 1 },
-  ],
-};
-export const initialPosition: string[][] = [
-  ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
-  ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
-  Array(8).fill(""),
-  Array(8).fill(""),
-  Array(8).fill(""),
-  Array(8).fill(""),
-  ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
-  ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
-];
 
 export const checkForValidClick = (event: React.MouseEvent) => {
   const { clientX, clientY, currentTarget } = event;
@@ -128,6 +23,5 @@ export const checkForValidClick = (event: React.MouseEvent) => {
 
   return { isValid, row, col };
 };
-export const emptyPiece: string = "";
 export const boardSize: number = 600;
 export const squareSize: number = boardSize / 8;
