a0f413f5e2664e852c3da8bfc59d480c63fe2339
parteek malik <parteekmalik5@gmail.com>
2023-10-08 16:28:59 +0530

    MOVED TO VITE

--- src/components/Live/Live.tsx
+++ src/components/Live/Live.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Board from '../board/board'
+import { useParams } from 'react-router-dom'
 
 function Live() {
+  const{gameid} = useParams();
   return (
     <>
+    <div className='bg-gray-600 text-white text-3xl p-4'>User: {gameid}</div>
     <Board />
     </>
   )
