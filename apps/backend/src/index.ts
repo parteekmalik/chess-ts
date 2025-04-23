6ab151ee4c4f53dc32ef186191369be94a524adc
parteekmalik <parteekmalik5@gmail.com>
2025-04-23 14:16:25 +0530

    fix: test

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -8,20 +8,21 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors());
-
-const server = http.createServer();
 app.use(cookieParser());
 
-// Initialize socket server
 app.use((req, res, next) => {
   console.log(req.url);
   next();
 });
+
 app.use(apiRouter);
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+const server = http.createServer(app);
 
 new GameSocket(server);
 
-const PORT = env.PORT;
-server.listen(PORT, () => {
+const PORT = env.PORT || 3000;
+server.listen(PORT, env.NODE_ENV === "production" ?'0.0.0.0':"127.0.0.1", () => {
   console.log(`Server listening on port ${PORT}`);
 });
