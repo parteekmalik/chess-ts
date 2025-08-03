79cdafd10c21fef5736260f7747a06176496af42
Parteek malik <64765095+parteekmalik@users.noreply.github.com>
2025-08-03 23:36:00 +0530

    feat: clock icon

--- apps/web/src/app/play/live/[matchId]/_components/TimeContainerBar.tsx
+++ apps/web/src/app/play/live/[matchId]/_components/TimeContainerBar.tsx
@@ -48,11 +48,18 @@ const TimerComponent = ({ time, variant, isTurn }: { time: number; variant: "whi
     <span>{String(moment.duration(time).minutes())}</span>
   );
   const seconds = <span>{String(moment.duration(time).seconds()).padStart(2, "0")}</span>;
-  const milliseconds = isMicroSec && <span>.{String(moment.duration(time).milliseconds()).padStart(3, "0")}</span>;
+  const milliseconds = isMicroSec && <span>.{String(Math.floor(moment.duration(time).milliseconds() / 1000))}</span>;
   return (
     <div className={!isTurn ? "opacity-80" : ""}>
-      <div className={cn("flex w-[160px] items-center rounded-md px-4 py-1", variant === "white" ? "bg-white text-black" : "bg-black text-white")}>
-        <p className={cn("ml-auto text-2xl", isTurn && isMicroSec && "text-red-400")} style={{ fontFamily: "monospace" }}>
+      <div
+        className={cn(
+          "flex w-[165px] items-center justify-between rounded-md px-3 py-1",
+          variant === "white" ? "bg-white text-black" : "bg-black fill-white text-white",
+          isTurn && isMicroSec && "fill-red-400 text-red-400",
+        )}
+      >
+        <ClockSvg isTurn={isTurn} time={time} />
+        <p className={cn("ml-auto text-2xl")} style={{ fontFamily: "monospace" }}>
           {hour}
           {minute}:{seconds}
           {milliseconds}
@@ -60,4 +67,32 @@ const TimerComponent = ({ time, variant, isTurn }: { time: number; variant: "whi
       </div>
     </div>
   );
+};
+
+const ClockSvg = ({ isTurn, time }: { isTurn: boolean; time: number }) => {
+  const [rotation, setRotation] = React.useState(0);
+  useEffect(() => {
+    if (isTurn) {
+      const interval = setInterval(() => {
+        if (time <= 0) {
+          clearInterval(interval);
+          return;
+        }
+        setRotation((prev) => prev + 90);
+      }, 1000);
+      return () => clearInterval(interval);
+    } else {
+      setRotation(0);
+    }
+  }, [isTurn, time]);
+
+  if (isTurn)
+    return (
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" style={{ transform: `rotate(${rotation}deg)` }}>
+          <path d="M5.48,9a.93.93,0,0,0-.3.71v.58a.94.94,0,0,0,.3.71,1,1,0,0,0,.71.3h4.58a1,1,0,0,0,.71-.3.94.94,0,0,0,.29-.71V9.7A.92.92,0,0,0,11.48,9a1,1,0,0,0-.71-.27H6.19A1,1,0,0,0,5.48,9Z"></path>
+          <path d="M19.22,6.1a9.9,9.9,0,0,0-2.14-3.18A10.23,10.23,0,0,0,13.9.78,9.76,9.76,0,0,0,10,0,9.86,9.86,0,0,0,6.1.78,10,10,0,0,0,.78,6.1,9.81,9.81,0,0,0,0,10a9.81,9.81,0,0,0,.78,3.9A10,10,0,0,0,6.1,19.22,9.86,9.86,0,0,0,10,20a9.76,9.76,0,0,0,3.89-.78,10.23,10.23,0,0,0,3.18-2.14,9.9,9.9,0,0,0,2.14-3.18A9.81,9.81,0,0,0,20,10,9.81,9.81,0,0,0,19.22,6.1ZM17.07,13a7.65,7.65,0,0,1-1.65,2.42A7.81,7.81,0,0,1,13,17.06a7.46,7.46,0,0,1-3,.6,7.51,7.51,0,0,1-3-.6,7.74,7.74,0,0,1-2.43-1.65A8,8,0,0,1,2.94,13a7.46,7.46,0,0,1-.6-3,7.46,7.46,0,0,1,.6-3A8,8,0,0,1,4.58,4.59,7.74,7.74,0,0,1,7,2.94a7.51,7.51,0,0,1,3-.6,7.45,7.45,0,0,1,3,.6,7.74,7.74,0,0,1,2.43,1.65A7.65,7.65,0,0,1,17.07,7a7.46,7.46,0,0,1,.6,3A7.46,7.46,0,0,1,17.07,13Z" />
+        </svg>
+      </div>
+    );
 };
